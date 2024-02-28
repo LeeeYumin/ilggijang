500bd5428c033be598af2dfe15753d08ff01b3ec
rebeca3081 <hyoeun3081@gmail.com>
2024-02-28 14:22:40 +0900

    240228_server(node.js)수정_장효은

---
+++ client/src/layouts/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="footer">
+        <logo/>
+        <table>
+            <tr>
+                <td>ⓒcopyright 2024 읽기장</td>
+            </tr>
+            <tr>
+                <td>주소 :</td>
+                <td></td>
+                <td>연락처 :</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>개발자이름 : 장효은, 신수지, 이유민, 도승민, 박지웅</td>
+            </tr>
+        </table>
+        <customercenter/>
+    </div>
+</template>
+
+<style scoped>
+#footer {
+    border: 1px solid #888;
+}
+table, tr, td {
+    border: none;
+}
+</style>
