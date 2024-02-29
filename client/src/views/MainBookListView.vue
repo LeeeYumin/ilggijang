0b81fe904abd7e0226b7e44bc9a7a8e16addacc6
zu9595 <qkrwldnddl02@naver.com>
2024-03-01 00:19:21 +0900

    240229h_메인도서목록미완성

--- client/src/views/MainBookListView.vue
+++ client/src/views/MainBookListView.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="books">
-    <div class="booklist" :key="j" v-for="(book, j) in bookList">
+    <h3>도서 목록</h3>
+    <div class="booklist" :key="i" v-for="(book, i) in bookList">
       <b-card :title="book.book_name" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
-        tag="article" style="max-width: 20rem;" class="mb-2" v-if="j <= 7">
+        tag="article" style="max-width: 20rem;" class="mb-2">
         <b-card-text>
           <p>{{ book.category_code }}</p>
           <p>{{ book.title }}</p>
@@ -11,26 +12,53 @@
       </b-card>
     </div>
   </div>
+  <!-- <div class="books">
+    <div class="booklist" :key="i" v-for="(book2, i) in bookList2">
+      <b-card :title="book2.book_name" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
+        tag="article" style="max-width: 20rem;" class="mb-2">
+        <b-card-text>
+          <p>{{ book2.category_code }}</p>
+          <p>{{ book2.title }}</p>
+        </b-card-text>
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+      </b-card>
+    </div>
+  </div> -->
 </template>
 
 <script>
 import axios from 'axios';
 
+
 export default {
   data() {
     return {
       bookList: []
+      // , bookList2: []
     }
   },
   created() {
     this.getBookList();
+  },
+  computed: {
+    
   },
   methods: {
     async getBookList() {
       let result = await axios.get('/api/books')
         .catch(err => console.log(err));
 
-      this.bookList = result.data;
+      this.makeBookList(result.data);
+    },
+    makeBookList(json) {
+      for (let i = 0; i < 8; i++) {
+        // if(i < 4){
+          this.bookList[i] = json[i];
+        // }
+        // else{
+        //   this.bookList2[i] = json[i];
+        // }
+      }
     }
   }
 }
@@ -38,10 +66,11 @@ export default {
 
 <style scoped>
 .books {
+  text-align: center;
   margin: 0 auto;
 }
+
 .booklist {
   display: inline-block;
 }
-
 </style>
