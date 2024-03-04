24b485475642a134da307227d425f5efeff68ce7
https://github.com/qqaza/webworkspace.git <tmdals0145@naver.com>
2024-03-04 09:03:33 +0900

    240304_로그인,회원리스트(도승민)

---
+++ client/src/views/NoticeView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <h1>공지사항</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>내용</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+</template>
